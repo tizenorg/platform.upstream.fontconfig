92ac2497588d43f3971cfdd178065de0233d2f5c
Deokjin Kim <deokjin81.kim@samsung.com>
2012-06-07 19:07:03 +0900

    Add TC for DTS
    Change-Id: I5f75b5d3c8fb63a685fbdd877ff6f191bd3f489e

---
+++ TC/testcase/utc_fc_list.c
@@ -0,0 +1,34 @@
+#include <tet_api.h>
+
+static void startup(void);
+static void cleanup(void);
+
+void (*tet_startup)(void) = startup;
+void (*tet_cleanup)(void) = cleanup;
+
+static void utc_fc_list(void);
+
+struct tet_testlist tet_testlist[] = {
+	{ utc_fc_list, 1 },
+	{ NULL, 0 },
+};
+
+static void startup(void)
+{
+	/* start of TC */
+}
+
+static void cleanup(void)
+{
+	/* end of TC */
+}
+
+static void utc_fc_list(void)
+{
+	int ret;
+	ret = system("fc-list");
+	if(WEXITSTATUS(ret) == 0)
+		dts_pass("utc_fc_list");
+	else
+		dts_fail("utc_fc_list");
+}
